3ab78abc844ab396f5c386113be2163be57e5b60
antonior24 <antonio.romero.ostos.al@iespoligonosur.org>
2025-10-20 14:42:06 +0200

    subrama sin terminar

--- componentes/views.py
+++ componentes/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def home(request):
     return render(request, 'componentes/home.html', {})
+
+def product_list(request):
+    productos = Product.objects.select_related("manufacturer").prefetch_related("categories")
+    productos = productos.all()
+    return render(request, 'componentes/product_list.html', {"productos_mostrar": productos})
