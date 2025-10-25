c0ddac6a76727cd6eaa92f4205e5c61b01bb4b72
antonior24 <antonio.romero.ostos.al@iespoligonosur.org>
2025-10-25 21:26:22 +0200

    commit de url2

--- componentes/urls.py
+++ componentes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.product_list, name='product_list'),
+    path('manufacturers/', views.manufacturers_list, name='manufacturers_list'),
 ]
--- componentes/views.py
+++ componentes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Manufacturer, Category, ProductCategory, Customer, CompanyInfo, Order, OrderItem
+from django.db.models import Q, Prefetch
 
 # Create your views here.
 def home(request):
@@ -9,3 +10,9 @@ def product_list(request):
     productos = Product.objects.select_related("manufacturer").prefetch_related("categories")
     productos = productos.all()
     return render(request, 'componentes/product_list.html', {"productos_mostrar": productos})
+
+def manufacturers_list(request):
+    fabricantes = Manufacturer.objects.prefetch_related("producto_manufacturer").order_by("name")
+    fabricantes = fabricantes.all()
+    return render(request, 'componentes/manufacturers_list.html', {"fabricantes_mostrar": fabricantes})
+
