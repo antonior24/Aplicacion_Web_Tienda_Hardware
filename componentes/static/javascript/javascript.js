501685587b1cab3ddd5f008445e23e1c8cdcc7d8
antonior24 <antonio.romero.ostos.al@iespoligonosur.org>
2025-11-13 22:03:29 +0100

    commit del logo y el archivo js

---
+++ componentes/static/javascript/javascript.js
@@ -0,0 +1,15 @@
+// que solamente al entrar a la pagina me pida si quiero entrar o no
+window.onload = function() {
+    // Si ya contestó antes, no preguntamos más
+    if (localStorage.getItem("accesoPermitido") === "true") {
+        return;
+    }
+
+    let aceptar = confirm("¿Desea entrar en la página?");
+    if (!aceptar) {
+        window.location.href = "https://www.google.com";
+    } else {
+        // Guardamos que ya aceptó
+        localStorage.setItem("accesoPermitido", "true");
+    }
+}
